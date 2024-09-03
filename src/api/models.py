88d97481bc6d8f62a6c5615eb10b04871ede492d
yonilhp <55214610+yonilhp@users.noreply.github.com>
2024-09-03 23:36:18 +0000

    Funciona registro y la pantalla

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     last_name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+ 
 
     def __repr__(self):
         return f'<User {self.email}>'
